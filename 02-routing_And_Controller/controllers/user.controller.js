83e10d574863cc3d26b32e18af1f99ce0936520f
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-09-24 13:14:08 +0530

    featur(02-roouter and controller) Implement DELETE /data endpoint to remove a user by ID

--- 02-routing_And_Controller/controllers/user.controller.js
+++ 02-routing_And_Controller/controllers/user.controller.js
@@ -1,9 +1,12 @@
-const user =[ {
-  FirstName: "Gopal",
-  LastName: "Shukla",
-  department: "BCA",
-  username: "gopalshukla0018",
-},]
+const user = [
+  {
+    id: 1,
+    FirstName: "Gopal",
+    LastName: "Shukla",
+    department: "BCA",
+    username: "gopalshukla0018",
+  },
+];
 
 export const getUserData = (_, res) => {
   return res.status(200).json({
@@ -12,11 +15,21 @@ export const getUserData = (_, res) => {
   });
 };
 
-export const createUserData=(req,res)=>{
-    const newUser=req.body;
-    user.push(newUser)
-    return res.status(200).json({
-        message:"user Create Successfully",
-        user:newUser,
-    })
-}
+export const createUserData = (req, res) => {
+  const newUser = req.body;
+  user.push(newUser);
+  return res.status(200).json({
+    message: "user Create Successfully",
+    user: newUser,
+  });
+};
+
+export const deleteUserData = (req, res) => {
+  const { id } = req.body;
+  const index = user.findIndex((u) => u.id === id);
+  user.splice(index, 1);
+  return res.status(200).json({
+    message: "user delete Successfully",
+    user,
+  });
+};
--- 02-routing_And_Controller/routes/user.Routes.js
+++ 02-routing_And_Controller/routes/user.Routes.js
@@ -1,9 +1,14 @@
-import { getUserData, createUserData } from "../controllers/user.controller.js";
+import {
+  getUserData,
+  createUserData,
+  deleteUserData,
+} from "../controllers/user.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/data", getUserData);
 router.post("/data", createUserData);
+router.delete("/data", deleteUserData);
 
 export default router;
