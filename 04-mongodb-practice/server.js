8e209d5887f13ef01ea8ded9663ae9e351f86ac4
Gopal shukla <gopalshukla0018@gmail.com>
2025-10-16 23:53:00 +0530

    Connected MongoDB successfully (Chapter 4 completed)

--- 04-mongodb-practice/server.js
+++ 04-mongodb-practice/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Connection Error:", err);
+  });
 
-app.get("/",(req,res)=>{
-    res.send("Hello from backend");
-})
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
